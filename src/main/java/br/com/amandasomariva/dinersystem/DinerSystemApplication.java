fb4255e0f7467b6452b1e14a6fa7852d3cba3c4f
Amanda Somariva <amanda_somariva@hotmail.com>
2019-09-29 17:35:32 -0300

    Inicio do projeto backend com Springboot

---
+++ src/main/java/br/com/amandasomariva/dinersystem/DinerSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.amandasomariva.dinersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinerSystemApplication.class, args);
+	}
+
+}
